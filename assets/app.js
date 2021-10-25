b96d5b958cf9abdc67ef4cba61eb88f681581407
Yumithecat <89903745+yumithecat@users.noreply.github.com>
2021-10-25 17:24:09 +0200

    Add files via upload

---
+++ assets/app.js
@@ -0,0 +1,20 @@
+document.getElementById("DSuccess").style.display = "none";
+
+function DSuccess() {
+    document.getElementById("DSuccess").style.display = "block";
+    setTimeout(function(){ document.getElementById("DSuccess").style.display = "none"; }, 3000);
+}
+
+document.getElementById("DAbort").style.display = "none";
+
+function DAbort() {
+    document.getElementById("DAbort").style.display = "block";
+    setTimeout(function(){ document.getElementById("DAbort").style.display = "none"; }, 3000);
+}
+
+document.getElementById("Dn").style.display = "none";
+
+function Dn() {
+    document.getElementById("Dn").style.display = "block";
+    setTimeout(function(){ document.getElementById("Dn").style.display = "none"; }, 3000);
+}
